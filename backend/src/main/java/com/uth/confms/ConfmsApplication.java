02ebf72c1b7a003a446e2224f1fe1b48389dbab7
HoangThuc12 <hthuc961@gmail.com>
2026-01-01 21:47:50 +0700

    Update user/security structure

---
+++ backend/src/main/java/com/uth/confms/ConfmsApplication.java
@@ -0,0 +1,13 @@
+package com.uth.confms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfmsApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/uth/confms/user/entity/Role.java
@@ -0,0 +1,9 @@
+package com.uth.confms.user.entity;
+
+public enum Role {
+    AUTHOR,
+    REVIEWER,
+    PC_MEMBER,
+    CHAIR,
+    ADMIN
+}
